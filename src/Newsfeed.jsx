4b027e5b828a29e14242fb322842c8a0d8e93a73
Daniil <rusm9snik2013@yandex.ru>
2021-02-28 22:15:20 +0300

    LineGraph

--- src/Newsfeed.jsx
+++ src/Newsfeed.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import './Newsfeed.css';
+import LineGraph from "./LineGraph";
 
 const Newsfeed = () => {
     return (
-        <div>
-
+        <div className="newsfeed">
+            <div className="newsfeed__container">
+                <div className="newsfeed__chartSection">
+                    <div className="newsfeed__portfolio">
+                        <h1>$1,145,656</h1>
+                        <p>+$446.63 (+0.04%) Today</p>
+                    </div>
+                    <div className="newsfeed__chart">
+                        <LineGraph />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
---
+++ src/Stats.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Stats.css';
+
+const Stats = () => {
+    return(
+        <div className="stats">
+            <div className="stats__container">
+                <div className="stats__header">
+                    <p>Stocks</p>
+                </div>
+                <div className="stats__content">
+                    <div className="stats__rows">
+
+                    </div>
+                </div>
+                <div className="stats__header">
+                    <p>Lists</p>
+                </div>
+                <div className="stats__rows">
+
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Stats;
